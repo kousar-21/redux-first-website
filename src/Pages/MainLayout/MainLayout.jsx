41e752cc510a84de575b88424f774d600e6a75ca
kousar-21 <kousarahammed5@gmail.com>
2025-09-15 22:01:09 +0600

    adding mamba ui in project

--- src/Pages/MainLayout/MainLayout.jsx
+++ src/Pages/MainLayout/MainLayout.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navbar from '../../component/Navbar/Navbar';
+import Footer from '../../component/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
-            This is main layout
+            <div className='bg-gray-800 mb-10'>
+                <Navbar></Navbar>
+            </div>
+            <div className='bg-gray-700'>
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
