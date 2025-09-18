6403cf018b341b7d79c3e71dbd8b8f84d3bdb939
kousar-21 <kousarahammed5@gmail.com>
2025-09-18 19:59:43 +0600

    adding modified navbar and footer

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleDarkMode } from '../redux/slices';
 
 const Navbar = () => {
+    const dispatch = useDispatch();
+    const { darkMode } = useSelector((state) => state.home);
+
+    const handleToggle = () => {
+        dispatch(toggleDarkMode());
+    };
     return (
-        <header className="p-4 dark:bg-gray-100 dark:text-gray-800">
-            <div className="container flex justify-between h-16 mx-auto">
-                <div className="flex">
-                    <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-8 h-8 dark:text-violet-600">
-                            <path d="M27.912 7.289l-10.324-5.961c-0.455-0.268-1.002-0.425-1.588-0.425s-1.133 0.158-1.604 0.433l0.015-0.008-10.324 5.961c-0.955 0.561-1.586 1.582-1.588 2.75v11.922c0.002 1.168 0.635 2.189 1.574 2.742l0.016 0.008 10.322 5.961c0.455 0.267 1.004 0.425 1.59 0.425 0.584 0 1.131-0.158 1.602-0.433l-0.014 0.008 10.322-5.961c0.955-0.561 1.586-1.582 1.588-2.75v-11.922c-0.002-1.168-0.633-2.189-1.573-2.742zM27.383 21.961c0 0.389-0.211 0.73-0.526 0.914l-0.004 0.002-10.324 5.961c-0.152 0.088-0.334 0.142-0.53 0.142s-0.377-0.053-0.535-0.145l0.005 0.002-10.324-5.961c-0.319-0.186-0.529-0.527-0.529-0.916v-11.922c0-0.389 0.211-0.73 0.526-0.914l0.004-0.002 10.324-5.961c0.152-0.090 0.334-0.143 0.53-0.143s0.377 0.053 0.535 0.144l-0.006-0.002 10.324 5.961c0.319 0.185 0.529 0.527 0.529 0.916z"></path>
-                            <path d="M22.094 19.451h-0.758c-0.188 0-0.363 0.049-0.515 0.135l0.006-0.004-4.574 2.512-5.282-3.049v-6.082l5.282-3.051 4.576 2.504c0.146 0.082 0.323 0.131 0.508 0.131h0.758c0.293 0 0.529-0.239 0.529-0.531v-0.716c0-0.2-0.11-0.373-0.271-0.463l-0.004-0.002-5.078-2.777c-0.293-0.164-0.645-0.26-1.015-0.26-0.39 0-0.756 0.106-1.070 0.289l0.010-0.006-5.281 3.049c-0.636 0.375-1.056 1.055-1.059 1.834v6.082c0 0.779 0.422 1.461 1.049 1.828l0.009 0.006 5.281 3.049c0.305 0.178 0.67 0.284 1.061 0.284 0.373 0 0.723-0.098 1.027-0.265l-0.012 0.006 5.080-2.787c0.166-0.091 0.276-0.265 0.276-0.465v-0.716c0-0.293-0.238-0.529-0.529-0.529z"></path>
-                        </svg>
-                    </a>
-                    <ul className="items-stretch hidden space-x-3 lg:flex">
-                        <li className="flex">
-                            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Link</a>
-                        </li>
-                        <li className="flex">
-                            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border- dark:text-violet-600 dark:border-violet-600">Link</a>
-                        </li>
-                        <li className="flex">
-                            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Link</a>
-                        </li>
-                        <li className="flex">
-                            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Link</a>
-                        </li>
+        <div className="navbar bg-base-100 rounded-box shadow-xl p-4">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">My App</a>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="User Avatar" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li><a>Profile</a></li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
                     </ul>
                 </div>
-                <div className="items-center flex-shrink-0 hidden lg:flex">
-                    <button className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Log in</button>
-                </div>
-                <button className="p-4 lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-800">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
-                </button>
+                <label className="swap swap-rotate btn btn-ghost btn-circle">
+                    {/* This hidden checkbox controls the theme toggle */}
+                    <input type="checkbox" onChange={handleToggle} checked={darkMode} />
+                    <svg className="swap-on fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM18.36,6.36a1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.41ZM12,19a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM7.05,18.95a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.41Zm8.9-13.9a1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,15.64,4l-.71.71A1,1,0,0,0,15.9,5.05ZM17,12a1,1,0,0,0,1,1h1a1,1,0,0,0,0-2H18A1,1,0,0,0,17,12ZM12,15a3,3,0,1,0-3-3A3,3,0,0,0,12,15Z" /></svg>
+                    <svg className="swap-off fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.64-1.39l-4.04,4.04A5.5,5.5,0,0,1,10.23,3.77,5.5,5.5,0,0,1,19.33,12l4.04-4.04A1,1,0,0,0,21.64,13Zm-17,6a1,1,0,0,0-1-1h-1a1,1,0,0,0,0,2h1A1,1,0,0,0,4.64,19Zm0-6a1,1,0,0,0-1-1h-1a1,1,0,0,0,0,2h1A1,1,0,0,0,4.64,13Zm0-6a1,1,0,0,0-1-1h-1a1,1,0,0,0,0,2h1A1,1,0,0,0,4.64,7Zm11.03-3a1,1,0,0,0-1-1h-1a1,1,0,0,0,0,2h1A1,1,0,0,0,15.67,4Zm0-6a1,1,0,0,0-1-1h-1a1,1,0,0,0,0,2h1A1,1,0,0,0,15.67-2Zm-11.03-3a1,1,0,0,0-1-1h-1a1,1,0,0,0,0,2h1A1,1,0,0,0,4.64-2ZM19.33,12a5.5,5.5,0,0,1-9.1-.56A1,1,0,0,0,10.23,12.5a5.5,5.5,0,0,1,9.1.56A1,1,0,0,0,19.33,12Z" /></svg>
+                </label>
             </div>
-        </header>
+        </div>
     );
 };
 
